5b7f9972f73736198527c09d960fe10de98828f9
juliusz brodnicki <juliusbrodnicki@gmail.com>
2023-12-05 21:53:55 +0100

    Day 1 - Exercise 2

--- Day1/day1.c
+++ Day1/day1.c
@@ -1,5 +1,75 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#define ZERO_LEN 4
+#define ONE_LEN 3
+#define TWO_LEN 3
+#define THREE_LEN 5
+#define FOUR_LEN 4
+#define FIVE_LEN 4
+#define SIX_LEN 3
+#define SEVEN_LEN 5
+#define EIGHT_LEN 5
+#define NINE_LEN 4
+#define RES 54591
+
+const char *numbers[] = {
+        "zero", "one", "two" , "three", "four", "five", "six", "seven", "eight", "nine"
+};
+
+int get_number_from_letters(const char *line, ssize_t pos, ssize_t line_size, int index, size_t len){
+    if(pos+len < line_size) {
+        int res = strncmp(numbers[index], (line + pos), len);
+        if (res == 0)
+            return index;
+    }
+    return -1;
+}
+
+int number_from_letters(const char *line, ssize_t pos, ssize_t line_size){
+    char c = *(line+pos);
+    int res;
+    switch (c) {
+        case 'z':{
+            return get_number_from_letters(line, pos, line_size, 0, ZERO_LEN);
+        }
+        case 'o': {
+            return get_number_from_letters(line, pos, line_size, 1, ONE_LEN);
+        }
+        case 't':{
+            res = get_number_from_letters(line, pos, line_size, 2, TWO_LEN);
+            if(res == -1)
+                return get_number_from_letters(line, pos, line_size, 3, THREE_LEN);
+            else
+                return res;
+        }
+        case 'f':{
+            res = get_number_from_letters(line, pos, line_size, 4, FOUR_LEN);
+            if(res == -1)
+                return get_number_from_letters(line, pos, line_size, 5, FIVE_LEN);
+            else
+                return res;
+        }
+        case 's':{
+            res = get_number_from_letters(line, pos, line_size, 6, SIX_LEN);
+            if(res == -1)
+                return get_number_from_letters(line, pos, line_size, 7, SEVEN_LEN);
+            else
+                return res;
+        }
+        case 'e':{
+            return get_number_from_letters(line, pos, line_size, 8, EIGHT_LEN);
+        }
+        case 'n':{
+            return get_number_from_letters(line, pos, line_size, 9, NINE_LEN);
+        }
+        default:
+            break;
+    }
+
+    return -1;
+}
+
 
 int sum_from_file(char  *filename){
     FILE *f = fopen(filename, "r");
@@ -13,11 +83,17 @@ int sum_from_file(char  *filename){
 
     while ((line_size = getline(&line, &len, f)) != -1) {
         for (int i = 0; i < line_size; ++i) {
-            if(number1 == -1 && isdigit(*(line+i))){
-                number1 = ((int)*(line+i))%48;
+            if(number1 == -1){
+                if(isdigit(*(line+i)))
+                    number1 = ((int)*(line+i))%48;
+                else
+                    number1 = number_from_letters(line, i, line_size);
             }
-            if(number2 == -1 && isdigit(*(line+(line_size-1-i)))){
-                number2 = ((int)*(line+((line_size-1-i))))%48;
+            if(number2 == -1){
+                if(isdigit(*(line+(line_size-1-i))))
+                    number2 = ((int)*(line+((line_size-1-i))))%48;
+                else
+                    number2 = number_from_letters(line, line_size-1-i, line_size);
             }
             if(number1 != -1 && number2 != -1)
                 break;
