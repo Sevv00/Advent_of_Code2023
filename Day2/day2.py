399fba22ded0838b9350ed9df10f8317b7f0484f
juliusz brodnicki <juliusbrodnicki@gmail.com>
2023-12-06 15:23:00 +0100

    Day 2 - Exercise 1

---
+++ Day2/day2.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def getGamesHistory(filename):
+    games_id = list()
+    games_results = list()
+    num = 0
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            x = line.split(':', 1)
+            games_id.append(int((x[0].split(" "))[1]))
+            draw_results = (x[1].split(';'))
+            game_res = np.zeros(shape=(len(draw_results),3))
+            draw_count = 0
+            for draw in draw_results:
+                col = draw.strip().split(' ')
+                counter = 0
+                for c in col:
+                    if(counter%2 == 0):
+                        num = int(c)
+                    else:
+                        if c[0] == 'r':
+                            game_res[draw_count][0] = num
+                        elif c[0] == 'g':
+                           game_res[draw_count][1] = num
+                        else:
+                            game_res[draw_count][2] = num
+                    counter += 1
+                draw_count += 1
+            games_results.append(game_res)
+    return games_id, games_results
+
+
+def getIDSumWithPossibleGames(games_id, games_results, bag_content):
+    sum = 0
+    size = len(game_id)
+    for i in range(size):
+        max_col = np.max(games_results[i], 0)
+        if np.all(np.greater_equal(bag_content, max_col)):
+            sum += games_id[i]
+    return sum
+        
+        
+            
+
+
+
+game_id, game_res = getGamesHistory("Day2\day2.txt")
+
+bag = [12, 13, 14]
+
+sum = getIDSumWithPossibleGames(game_id, game_res, bag)
+
+print(sum)
+
+    
+                        
+
+
+            
+            
